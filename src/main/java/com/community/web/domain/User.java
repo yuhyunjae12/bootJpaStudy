865d8fb777bc914212d4171e74f13e2cf2c11b23
yuhyunjae12 <yuhyunjae12@naver.com>
2019-02-16 15:14:53 +0900

    test commit

---
+++ src/main/java/com/community/web/domain/User.java
@@ -0,0 +1,43 @@
+package com.community.web.domain;
+
+import com.community.web.domain.enums.SocialType;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // IDENTITY: 키 생성을 DB에 위임
+    private Long index;
+    private String name;
+    private String password;
+    private String email;
+    // LocalDateTime: 1.8부터 추가된 API로 Date, Calendar에서 부실한 날짜 연산기능을 추가로 제공
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+
+    // OAuth
+    private String principal; // <- 추가
+    private SocialType socialType; // <- 추가
+
+    @Builder
+    public User(String name, String password, String email, LocalDateTime createdDate, LocalDateTime updatedDate, String principal, SocialType socialType) {
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.createdDate = createdDate;
+        this.updatedDate = updatedDate;
+        this.principal = principal;
+        this.socialType = socialType;
+    }
+}
